64f86942baa0ddaab33d9c77b68a273b76b76737
prototowb <prototowb@gmail.com>
2021-06-09 23:27:58 +0200

    frontpage ready

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './global-styles';
+import { App } from './App';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+}
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </>, 
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
